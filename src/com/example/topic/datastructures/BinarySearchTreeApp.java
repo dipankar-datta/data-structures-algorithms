d3c2bacbf67f3f157ba37d1f1919d0e1e6747f93
dipankar-datta <dattapro5@gmail.com>
2019-07-23 22:34:01 +0530

    Binary Search Tree. Implemented remove and delete. Both work same. delete is self implemented.

--- src/com/example/topic/datastructures/BinarySearchTreeApp.java
+++ src/com/example/topic/datastructures/BinarySearchTreeApp.java
@@ -12,8 +12,10 @@ public class BinarySearchTreeApp {
         binarySearchTree.add(15, "Fifteen");
         binarySearchTree.add(1, "One");
         System.out.println("Done");
+        binarySearchTree.delete(20);
 
         System.out.println(binarySearchTree.get(55));
+        System.out.println(binarySearchTree.get(20));
     }
 
 
@@ -39,13 +41,16 @@ class BinarySearchTree<K extends Object, V> {
                         break;
                     }
                     currentNode = currentNode.getLeft();
-                } else {
+                } else if (newNode.getKey().hashCode() > currentNode.getKey().hashCode()){
                     // RIGHT
                     if (currentNode.getRight() == null) {
                         currentNode.setRight(newNode);
                         break;
                     }
                     currentNode = currentNode.getRight();
+                } else if (newNode.getKey().hashCode() == currentNode.getKey().hashCode()){
+                    // MATCH
+                    currentNode.setValue(newNode.getValue());
                 }
             }
         }
@@ -74,6 +79,142 @@ class BinarySearchTree<K extends Object, V> {
         }
 
         return null;
+    }
+
+    // This method is copied from tutorial.
+    public void remove(K key) {
+        if (root == null) {
+            throw new RuntimeException("Cannot delete. Tree is empty");
+        }
+
+        Node currentNode = root;
+        Node parentNode = null;
+
+        while (true) {
+            if (key.hashCode() < currentNode.getKey().hashCode()) {
+                // LEFT
+                currentNode = currentNode.getLeft();
+                parentNode = currentNode;
+            } else if (key.hashCode() > currentNode.getKey().hashCode()){
+                // RIGHT
+                currentNode = currentNode.getRight();
+                parentNode = currentNode;
+            } else if (key.hashCode() == currentNode.getKey().hashCode()){
+                // Condition 1: When no right child
+                if (currentNode.getRight() == null) {
+                    if (parentNode == null) {
+                        root = currentNode.getLeft();
+                    } else {
+                        // If parent > current value, make current left child a child of parent
+                        if (currentNode.getKey().hashCode() < parentNode.getKey().hashCode()) {
+                            parentNode.setLeft(currentNode.getLeft());
+
+                            // If parent < current, make left child a right child of parent
+                        } else if (currentNode.getKey().hashCode() > parentNode.getKey().hashCode()) {
+                            parentNode.setRight(currentNode.getLeft());
+                        }
+                    }
+                // Condition 2: Right child which doesn't have a left child
+                } else if (currentNode.getRight().getLeft() == null) {
+                    if (parentNode == null) {
+                        root = currentNode.getLeft();
+                    } else {
+                        currentNode.getRight().setLeft(currentNode.getLeft());
+
+                        // If parent > current, make right child of the left the parent
+                        if (currentNode.getKey().hashCode() < parentNode.getKey().hashCode()) {
+                            parentNode.setLeft(currentNode.getRight());
+                        // If parent < current, make right child a right child of the parent
+                        } else if (currentNode.getKey().hashCode() > parentNode.getKey().hashCode()) {
+                            parentNode.setRight(currentNode.getRight());
+                        } else {
+
+                            // Find the right child's left most child
+                            Node leftMostNode = currentNode.getRight().getLeft();
+                            Node leftMostParentNode = currentNode.getRight();
+
+                            while (leftMostNode.getLeft() != null) {
+                                leftMostParentNode = leftMostNode;
+                                leftMostNode = leftMostNode.getLeft();
+                            }
+
+                            // Parent's left subtree is now leftmost's right subtree
+                            leftMostParentNode.setLeft(leftMostNode.getRight());
+                            leftMostNode.setLeft(currentNode.getLeft());
+                            leftMostNode.setRight(currentNode.getRight());
+
+                            if (parentNode == null) {
+                                root = leftMostNode;
+                            } else {
+                                if (currentNode.getKey().hashCode() < parentNode.getKey().hashCode()) {
+                                    parentNode.setLeft(leftMostNode);
+                                } else if (currentNode.getKey().hashCode() > parentNode.getKey().hashCode()) {
+                                    parentNode.setRight(leftMostNode);
+                                }
+                            }
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    // This method is self implemented.
+    public void delete(K key) {
+        Node currentNode = root;
+        Node parentNode = null;
+
+        while (currentNode != null) {
+            if (key.hashCode() > currentNode.getKey().hashCode()) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (key.hashCode() < currentNode.getKey().hashCode()) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (key.hashCode() == currentNode.getKey().hashCode()) {
+
+                // Condition 1 : When Node has no children;
+                if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+                    // If current node is left of parent, then we set left of parent null
+                    if (parentNode.getLeft().getKey().hashCode() == currentNode.getKey().hashCode()) {
+                        parentNode.setLeft(null);
+
+                    // If current node is right of parent, then we set right of parent null
+                    } else if (parentNode.getRight().getKey().hashCode() == currentNode.getKey().hashCode()) {
+                        parentNode.setRight(null);
+                    }
+                // Condition 2 : When Node has one child;
+                } else if ((currentNode.getLeft() != null && currentNode.getRight() == null) ||
+                        (currentNode.getLeft() == null && currentNode.getRight() != null)) {
+                    // If current node is left of parent, then we set left of parent null
+                    parentNode.setRight(currentNode.getRight());
+                    parentNode.setLeft(currentNode.getLeft());
+
+                // Condition 3 : When Node has two children
+                } else if (currentNode.getLeft() != null && currentNode.getRight() != null) {
+                    Node leftLeafNode = currentNode;
+                    Node parentLeftLeafNode = null;
+
+                    while (leftLeafNode.getLeft() != null) {
+                        parentLeftLeafNode = leftLeafNode;
+                        leftLeafNode = leftLeafNode.getLeft();
+                    }
+
+                    // If current node is left of parent, then we set left of parent
+                    if (parentNode.getLeft().getKey().hashCode() == currentNode.getKey().hashCode()) {
+                        parentNode.setLeft(leftLeafNode);
+
+                    // If current node is right of parent, then we set right of parent
+                    } else if (parentNode.getRight().getKey().hashCode() == currentNode.getKey().hashCode()) {
+                        parentNode.setRight(leftLeafNode);
+                    }
+
+                    parentLeftLeafNode.setLeft(null);
+                }
+                return;
+            }
+        }
     }
 
     private class Node<K extends Object, V> {
